0005a598b92fcf5a9ee70c194b3e969a67e2fba4
Boundy2739 <boundy42@gmail.com>
2026-03-13 19:23:08 +0000

    Added navigation bar

--- myaccount.php
+++ myaccount.php
@@ -1,33 +1,40 @@
 <?php
 require_once 'pdo.php';
 session_start();
-if($_SESSION['authorised'] !== TRUE){
+if ($_SESSION['authorised'] !== TRUE) {
     header('Location: index.php');
     exit;
-}
-else{
+} else {
     $_SESSION['login'] = TRUE;
-
 }
-$_SESSION['account_id'] = $_GET['account'];
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Title</title>
 </head>
+
 <body>
-    <h1>Cash transfer app</h1>
-    <h2>Options menu</h2>
+    <nav class="navigation-bar">
+        <a href="myaccount.php">Dashboard</a>
+        <a href="accountslist.php">Accounts</a>
+        <a href="sendmoney.php">Send Money</a></li>
+        <a href="view_transactions.php">Transactions</a>
+        <a href="profile.php">Profile</a>
+        <a href="logout.php">Logout</a>
+    </nav>
+
     <?php
-    $sql = "SELECT * from accounts where account_id = :id";
+    $sql = "SELECT firstname from users where id = :id";
     $stmt = $pdo->prepare($sql);
-    $stmt->execute([':id'=>$_GET['account']]);
+    $stmt->execute([':id' => $_SESSION['user_id']]);
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
-    echo 'Greetings '. $user['firstname'] .' !';
+    echo 'Greetings ' . $user['firstname'] . ' !';
     print_r($_SESSION);
     ?>
     <section class="user-options">
@@ -42,4 +49,5 @@ $_SESSION['account_id'] = $_GET['account'];
         </ul>
     </section>
 </body>
+
 </html>
