24b5bdea45a03e9786a212f8622e834546e550ac
Boundy2739 <boundy42@gmail.com>
2026-03-14 16:56:20 +0000

    Added some styling and other small changes

--- walletoptions.php
+++ walletoptions.php
@@ -2,17 +2,17 @@
 require_once 'pdo.php';
 session_start();
 if ($_SESSION['authorised'] !== TRUE) {
-    header('Location: index.php');
+    header('Location: myaccount.php');
     exit;
-} else {
-    $_SESSION['login'] = TRUE;
 }
+ 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
@@ -29,7 +29,19 @@ if ($_SESSION['authorised'] !== TRUE) {
         <ul>
             <li><a href="add_funds.php">Add funds</a></li>
             <li><a href="transfer.php">Transfer</a></li>
-            <li><a href="setdefaultaccount.php">Set account as default/a></li>
+            <?php 
+            $sql = "SELECT is_default from accounts where account_id =:id";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([':id'=>$_GET['account']]);
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            print_r($result);
+
+            if($result['is_default'] === 0){
+                echo'<li><a href="setdefaultaccount.php?account='.$_GET['account'].'">Set account as default</a></li>';
+
+            };
+            ?>
+            
         </ul>
     </section>
     
