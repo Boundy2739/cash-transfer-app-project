0435f03ed984d6737714101e51906449125c02fe
Boundy2739 <boundy42@gmail.com>
2026-03-14 17:50:01 +0000

    Add initial script for transfers between user accounts

--- transfer.php
+++ transfer.php
@@ -1,3 +1,44 @@
+<?php
+require_once 'pdo.php';
+require_once 'check_positive_balance.php';
+session_start();
+if($_SESSION['authorised'] !== TRUE || empty($_SESSION['current_account'])){
+    header('Location: walletoptions.php');
+    exit;
+
+}
+$sql = "SELECT * from accounts where owner_id =:id";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([':id'=>$_SESSION['user_id']]);
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if($_SERVER['METHOD' == 'POST']){
+    $amount = filter_input(INPUT_POST, 'amount', FILTER_VALIDATE_FLOAT);
+    if($amount === false || $amount < 0){
+        die('Invalid amount.');
+    }
+
+    
+    $sql = "SELECT * accounts where account_id =:id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([':id'=>$_SESSION['current_account']]);
+    $currentAcc = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+    $sql = "SELECT * from accounts where account_id =:id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([':id'=>$_POST['chosen-account']]);
+    $chosenAcc = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $funds = checkFunds($sender['balance'],$amount);
+
+    $currentAcc['balance'] = $currentAcc['balance'] - 
+ 
+
+
+};
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,6 +53,12 @@
         <label for="chosen-account">Choose an account</label>
         <select name="chosen-account" id="chosen-account">
             <option value="">Choose an account</option>
+            <?php
+            foreach ($rows as $row) {
+                echo "<option value='" . htmlspecialchars($row['account_id']) . "'>" . htmlspecialchars($row['account_name']) . "</option>";
+            }
+
+            ?>
             
         </select>
         <label for="amount">Amount</label>
