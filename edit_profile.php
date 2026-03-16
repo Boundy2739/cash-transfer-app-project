0d48a2e6367a80b4b52d6c116ba120311749d364
Boundy2739 <boundy42@gmail.com>
2026-03-16 19:35:19 +0000

    Add form validation

--- edit_profile.php
+++ edit_profile.php
@@ -6,57 +6,72 @@ if (!isset($_SESSION['authorised']) || $_SESSION['authorised'] !== TRUE) {
     exit;
 }
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $fname = trim($_POST['firstname'] ?? '');
+    $mname = trim($_POST['middlename'] ?? '');
+    $lname = trim($_POST['lastname'] ?? '');
+    $email = filter_input(INPUT_POST,$_POST['email'], FILTER_VALIDATE_EMAIL);
+    $number = filter_input(INPUT_POST,$_POST['phonenumber'], FILTER_VALIDATE_INT);
+    $street = $_POST['address1'];
+    $houseNumber = filter_input(INPUT_POST,$_POST['address2'], FILTER_VALIDATE_INT);
+    $dob = $_POST['dob'];
+    $date = DateTime::createFromFormat('Y-m-d', $dob);
+    $city = preg_replace('/[^a-zA-Z\s]/', '', $_POST['city'] ?? '');
+    $city = preg_replace('/\s+/', ' ', trim($city));
+    $postcode = preg_replace('/[^a-zA-Z0-9\s]/', '',$_POST['postcode']);
+    $postcode = preg_replace('/\s+/', ' ', trim($postcode));
+
+
     $fields = [];
     $params = [':id' => $_SESSION['user_id']];
 
-    if (!empty($_POST['firstname'])) {
+    if (!empty($_POST['firstname']) && preg_match('/^[a-zA-Z]+$/', $fname)) {
         $fields[] = "firstname = :firstname";
-        $params[':firstname'] = $_POST['firstname'];
+        $params[':firstname'] = $fname;
     }
 
-    if (!empty($_POST['middlename'])) {
+    if (!empty($_POST['middlename']) && preg_match('/^[a-zA-Z]+$/', $mname)) {
         $fields[] = "middlename = :middlename";
-        $params[':middlename'] = $_POST['middlename'];
+        $params[':middlename'] = $mname;
     }
 
-    if (!empty($_POST['surname'])) {
+    if (!empty($_POST['surname']) && preg_match('/^[a-zA-Z]+$/', $lname)) {
         $fields[] = "lastname = :surname";
-        $params[':surname'] = $_POST['surname'];
+        $params[':surname'] = $lname;
     }
 
-    if (!empty($_POST['email'])) {
+    if (!empty($email)) {
         $fields[] = "email = :email";
-        $params[':email'] = $_POST['email'];
+        $params[':email'] = $email;
     }
 
-    if (!empty($_POST['number'])) {
+    if (!empty($number)) {
         $fields[] = "phone = :phone";
-        $params[':phone'] = $_POST['number'];
+        $params[':phone'] = $number;
     }
 
-    if (!empty($_POST['address1'])) {
+    if (!empty($street) && preg_match('/^[a-zA-Z0-9\s]+$/', $street)) {
         $fields[] = "address_street_name = :address1";
-        $params[':address1'] = $_POST['address1'];
+        $params[':address1'] = $street;
     }
 
-    if (!empty($_POST['address2'])) {
+    if (!empty($houseNumber)) {
         $fields[] = "address_house_number = :address2";
-        $params[':address2'] = $_POST['address2'];
+        $params[':address2'] = $houseNumber;
     }
 
-    if (!empty($_POST['dob'])) {
+    if (!empty($dob) && $date && $date->format('Y-m-d') === $dob) {
         $fields[] = "date_of_birth = :dob";
-        $params[':dob'] = $_POST['dob'];
+        $params[':dob'] = $dob;
     }
 
     if (!empty($_POST['city'])) {
         $fields[] = "city = :city";
-        $params[':city'] = $_POST['city'];
+        $params[':city'] = $city;
     }
 
     if (!empty($_POST['postcode'])) {
         $fields[] = "postcode = :postcode";
-        $params[':postcode'] = $_POST['postcode'];
+        $params[':postcode'] = $postcode;
     }
     
 
