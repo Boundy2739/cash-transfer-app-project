006d678a8bd862a3ac41ca090f98a09125a9280b
Boundy2739 <boundy42@gmail.com>
2026-04-08 20:21:11 +0100

    Added attempts reset timer

--- login.php
+++ login.php
@@ -9,6 +9,12 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     exit();
 }
 $is_locked = is_locked($_SERVER['REMOTE_ADDR'],$pdo);
+if($is_locked === true){
+    $_SESSION['errorMessage'] = 'Too many attempts, retry later';
+    header("Location: index.php");
+    exit;
+
+}
 if (
     empty($_POST["email"]) ||
     empty($_POST["password"])
@@ -40,6 +46,7 @@ if($row && password_verify($password, $row['password_hash'])){
     $_SESSION['authorised'] = TRUE; /*This will variable will be used to check if the user logged in before doing any action */
     $_SESSION['user_id'] = $row['id'];/*This will hold the user's id and it will be used for furthermore verification */
     $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    reset_attempts($_SERVER['REMOTE_ADDR'],$pdo);
     header('Location: myaccount.php');
     exit;
 }
--- rate-limiter/ratelimiter.php
+++ rate-limiter/ratelimiter.php
@@ -1,10 +1,10 @@
 <?php
-
-function rate_limiter($key,$limit,$pdo)
+session_start();
+function rate_limiter($key, $limit, $pdo)
 {
 
     $ip = ip2long($key);
-
+    $currentTime = date("Y-m-d H:i:s");
     $sql = "SELECT * from rate_limit_ips where ip_address =:ip";
     $stmt = $pdo->prepare($sql);
     $stmt->execute([":ip" => $ip]);
@@ -13,33 +13,39 @@ function rate_limiter($key,$limit,$pdo)
 
     if (!$result) {
         //First login error
-        $sql = "INSERT into rate_limit_ips (ip_address, attempts,last_attempt) VALUES (:ip,:attempts,NOW()) ";
+        $sql = "INSERT into rate_limit_ips (ip_address, attempts,last_attempt) VALUES (:ip,:attempts,:last_attempt) ";
         $stmt = $pdo->prepare($sql);
         $stmt->execute(array(
             ":ip" => $ip,
             "attempts" => 1,
+            ":last_attempt" => $currentTime,
         ));
         return;
     }
 
-    $sql = "UPDATE rate_limit_ips SET attempts = attempts + 1, last_attempt = NOW() where ip_address =:ip";
+    $sql = "UPDATE rate_limit_ips SET attempts = attempts + 1, last_attempt =:last_attempt where ip_address =:ip";
     $stmt = $pdo->prepare($sql);
-    $stmt->execute([":ip" => $ip]);
+    $stmt->execute(array(":ip" => $ip, ":last_attempt" => $currentTime));
 
     if ($result['attempts'] >= $limit) {
-        $sql = "UPDATE rate_limit_ips SET last_attempt = NOW(), lock_time = NOW() WHERE ip_address = :ip";
+        $sql = "UPDATE rate_limit_ips SET last_attempt = :last_attempt, lock_time = :lock_time WHERE ip_address = :ip";
         $stmt = $pdo->prepare($sql);
-        $stmt->execute([":ip" => $ip]);
-        return "Too many attempts, retry later";
+        $stmt->execute(array(
+            ":ip" => $ip,
+            ":lock_time"=>$currentTime,
+            ":last_attempt"=>$currentTime
+        ));
+        return;
     }
 }
-function is_locked($key,$pdo){
+function is_locked($key, $pdo)
+{
     $ip = ip2long($key);
     $currentTime = time();
-
+    print_r($currentTime);
     $sql = "SELECT lock_time from rate_limit_ips WHERE ip_address =:ip";
     $stmt = $pdo->prepare($sql);
-    $stmt->execute([":ip"=>$ip]);
+    $stmt->execute([":ip" => $ip]);
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     if (!$result || !$result['lock_time']) {
         // ip not locked yet
@@ -47,12 +53,21 @@ function is_locked($key,$pdo){
     }
     $lockTime = strtotime($result['lock_time']);
 
-    if ($currentTime - $lockTime < 3600){
+    if ($currentTime - $lockTime < 60) {
+        $_SESSION['time'] = $currentTime;
+        $_SESSION['time2'] = $lockTime;
         //ip still locked
         return TRUE;
     }
 
-    return false; //timer expired
+    reset_attempts($key, $pdo);
+    return false; //timer expired attempts are reset
 
 }
-?>
+function reset_attempts($key, $pdo)
+{
+    $ip = ip2long($key);
+    $sql = "UPDATE rate_limit_ips SET attempts = 0, lock_time = NULL WHERE ip_address=:ip";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([":ip" => $ip]);
+}
