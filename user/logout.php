e28dbacfa85c47c7f241fd9c0512c7935008414f
Boundy2739 <boundy42@gmail.com>
2026-04-10 22:32:51 +0100

    Added new reflection

--- user/logout.php
+++ user/logout.php
@@ -1,9 +1,10 @@
 <?php 
 require_once "../config/config.php";
+require_once "../helpers/index.php";
 print_r($_SESSION);
 $_SESSION = [];
 print_r($_SESSION);
 session_destroy();
-header('Location: ../index.php');
+redirect();
 exit;
 ?>
